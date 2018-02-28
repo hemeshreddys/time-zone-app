852e5a42a00d51f1124699db9472347d041f4efd
hemesh <hemeshreddy@gmail.com>
2018-02-28 19:50:17 +1100

    added styling and components

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import DigitalTimeZone from './DigitalTimeZone';
 
 class App extends Component {
   render() {
+    const container = {
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+      border:'1px solid grey',
+      width:'100vw',
+      height:'100vh',
+      flexWrap:'wrap'
+  }
     return (
-      <div className="App">
-        <DigitalTimeZone tZone="US/Pacific"/>
+      <div style={container}>
+        <DigitalTimeZone tZone="US/Pacific" name="America"/>
+        <DigitalTimeZone tZone="Asia/Kolkata" name="India"/>
+        <DigitalTimeZone tZone="Australia/Melbourne" name="Melbourne"/>
       </div>
     );
   }
--- src/DigitalTimeZone.js
+++ src/DigitalTimeZone.js
@@ -9,11 +9,30 @@ class DigitalTimeZone extends Component{
             alignItems:'center',
             border:'1px solid grey',
             width:200,
-            height:200
+            height:200,
+            borderRadius:'100px',
+            flexDirection:'column',
+            margin:'2px',
+            backgroundColor:'#204E4A'
+        }
+        const itemsCountry ={
+            color:'#C5C3C6',
+            fontFamily: 'Anonymous Pro, monospace',
+            
+        }
+        const itemsTime ={
+            color:'#DCDCDD',
+            fontSize:'24px',
+            fontFamily: 'Julius Sans One, sans-serif',
         }
         return(
             <div style={container}>
-                <Clock format={'h:mm:ssa'} ticking={true} timezone={this.props.tZone} />
+                <div style={itemsCountry}>
+                    {this.props.name}
+                </div>
+                <div style={itemsTime}>
+                    <Clock format={'h:mm:ssa'} ticking={true} timezone={this.props.tZone} />
+                </div>
             </div>
         );
     }
